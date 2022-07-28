14d00f073ae927e865585f5b056d51af33a4d3e0
Nick Hoefle <nickhoefle@gmail.com>
2022-07-27 19:02:48 -0500

    Completed and tested Updating Shuttle Requirements section

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
 
-   let listedPlanets;
+
+
+
+window.addEventListener("load", function() {
+    let submitClicked = this.document.getElementById("formSubmit")
+    submitClicked.addEventListener("click", () => {
+    let list = document.getElementById("faultyItems")
+    let pilot = document.querySelector("input[name=pilotName").value.trim();
+    let copilot = document.querySelector("input[name=copilotName").value.trim()
+    let fuelLevel = document.querySelector("input[name=fuelLevel").value.trim();
+    let cargoLevel = document.querySelector("input[name=cargoMass").value.trim();  
+    formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+    
+
+    })
+
+
+    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
+    let listedPlanetsResponse;
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+    })
+    
+    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,6 +1,15 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
+// let list = document.getElementById("faultyItems");
+
+// console.log(list)
+
+
+
+
+
+
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
    /*
@@ -28,10 +37,93 @@ function validateInput(testInput) {
     }
     
 }
-console.log(validateInput('111t11'));
+
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
-}
+    if (validateInput(pilot) !== "Empty"){
+        list.innerHTML = `
+        <ol>
+            <li id="pilotStatus" data-testid="pilotStatus">Pilot ${pilot} ready to go.</li>
+        </ol>`
+        event.preventDefault();
+    } else { 
+        alert('Pilot Name required!')
+        event.preventDefault();
+    }  
+    
+    if (validateInput(copilot) !== "Empty"){
+        list.innerHTML = `
+        <ol>
+            <li id="pilotStatus" data-testid="pilotStatus">Pilot ${pilot} is ready for launch</li>
+            <li id="copilotStatus" data-testid="copilotStatus">Co-pilot ${copilot} is ready for launch</li>
+        </ol>`
+        event.preventDefault();
+    } else { 
+        alert('Co-Pilot Name required!')
+        event.preventDefault();
+    }  
+
+    if (fuelLevel < 10000 && cargoLevel < 10000 && (validateInput(pilot) !== "Empty" && validateInput(copilot) !== "Empty")){
+        list.style.visibility = 'visible';
+        let status = document.getElementById("launchStatus");
+        status.innerHTML = 'Shuttle not ready for launch'
+        status.style.color = "rgb(199, 37, 78)";
+        list.innerHTML = `
+        <ol>
+            <li id="pilotStatus" data-testid="pilotStatus">Pilot ${pilot} is ready for launch</li>
+            <li id="copilotStatus" data-testid="copilotStatus">Co-pilot ${copilot} is ready for launch</li>
+            <li id="fuelStatus" data-testid="fuelStatus">Fuel level too low for launch</li>
+            <li id="cargoStatus" data-testid="cargoStatus">Cargo mass low enough for launch</li>
+        </ol>`
+        event.preventDefault();
+    }
+    
+    if (fuelLevel < 10000 && cargoLevel > 10000 && (validateInput(pilot) !== "Empty" && validateInput(copilot) !== "Empty")){
+        list.style.visibility = 'visible';
+        let status = document.getElementById("launchStatus");
+        status.innerHTML = 'Shuttle not ready for launch'
+        status.style.color = "rgb(199, 37, 78)";
+        list.innerHTML = `
+        <ol>
+            <li id="pilotStatus" data-testid="pilotStatus">Pilot ${pilot} is ready for launch</li>
+            <li id="copilotStatus" data-testid="copilotStatus">Co-pilot ${copilot} is ready for launch</li>
+            <li id="fuelStatus" data-testid="fuelStatus">Fuel level too low for launch</li>
+            <li id="cargoStatus" data-testid="cargoStatus">Cargo mass too high for launch</li>
+        </ol>`
+        
+        event.preventDefault();
+    }
+
+    if (fuelLevel > 10000 && cargoLevel > 10000 && (validateInput(pilot) !== "Empty" && validateInput(copilot) !== "Empty")){
+        list.style.visibility = 'visible';
+        let status = document.getElementById("launchStatus");
+        status.innerHTML = 'Shuttle not ready for launch'
+        status.style.color = "rgb(199, 37, 78)";
+        list.innerHTML = `
+        <ol>
+            <li id="pilotStatus" data-testid="pilotStatus">Pilot ${pilot} is ready for launch</li>
+            <li id="copilotStatus" data-testid="copilotStatus">Co-pilot ${copilot} is ready for launch</li>
+            <li id="fuelStatus" data-testid="fuelStatus">Fuel level high enough for launch</li>
+            <li id="cargoStatus" data-testid="cargoStatus">Cargo mass too high for launch</li>
+        </ol>`
+        event.preventDefault();
+    }
+
+    if (fuelLevel > 10000 && cargoLevel < 10000 && (validateInput(pilot) !== "Empty" && validateInput(copilot) !== "Empty")){
+        list.style.visibility = 'visible';
+        let status = document.getElementById("launchStatus");
+        status.innerHTML = 'Shuttle is ready to launch'
+        status.style.color = "rgb(65, 159, 106)";
+        list.innerHTML = `
+        <ol>
+            <li id="pilotStatus" data-testid="pilotStatus">Pilot ${pilot} is ready for launch</li>
+            <li id="copilotStatus" data-testid="copilotStatus">Co-pilot ${copilot} is ready for launch</li>
+            <li id="fuelStatus" data-testid="fuelStatus">Fuel level high enough for launch</li>
+            <li id="cargoStatus" data-testid="cargoStatus">Cargo mass low enough for launch</li>
+        </ol>`
+        event.preventDefault();
+    }
+
+};
 
 async function myFetch() {
     let planetsReturned;
