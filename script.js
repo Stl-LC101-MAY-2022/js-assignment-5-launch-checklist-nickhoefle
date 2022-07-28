dddcf15cae940d4fa4dd64127bc2d205587426a9
Nick Hoefle <nickhoefle@gmail.com>
2022-07-28 14:57:17 -0500

    Coding complete. Begin refactoring and testing.

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 
 
 
-
-
 window.addEventListener("load", function() {
     let submitClicked = this.document.getElementById("formSubmit")
     submitClicked.addEventListener("click", () => {
@@ -13,21 +11,24 @@ window.addEventListener("load", function() {
     let fuelLevel = document.querySelector("input[name=fuelLevel").value.trim();
     let cargoLevel = document.querySelector("input[name=cargoMass").value.trim();  
     formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
-    
-
-    })
-
-
     let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    const listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
+        listedPlanets.json().then(function (json) {
+        listedPlanets = json;
+        let planet = pickPlanet(listedPlanets)
+        planet = listedPlanets[planet]
+        let name = planet.name;
+        let diameter = planet.diameter;
+        let star = planet.star;
+        let distance = planet.distance;
+        let moons = planet.moons;
+        let imageUrl = planet.image;
+        addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl)
+
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     })
-    
-    
+    })
+    })
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,28 +1,19 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
-// let list = document.getElementById("faultyItems");
-
-// console.log(list)
-
-
-
-
-
-
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
+    const missionTargetInsert = document.getElementById("missionTarget");
+    missionTargetInsert.innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">`
+
 }
 
 function validateInput(testInput) {
@@ -127,14 +118,13 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
 async function myFetch() {
     let planetsReturned;
-
-    planetsReturned = await fetch().then( function(response) {
-        });
-
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json")
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    let randomIndex = Math.floor(Math.random() * planets.length);
+    return randomIndex;
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
